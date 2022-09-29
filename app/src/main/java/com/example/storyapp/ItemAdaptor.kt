77bd36a1602efe97921b8cda14a69953a0f5d7e8
Amir Salimi <108394058+TheSalimi@users.noreply.github.com>
2022-09-29 14:45:26 +0330

    activity_details.xml , adaptor

---
+++ app/src/main/java/com/example/storyapp/ItemAdaptor.kt
@@ -0,0 +1,34 @@
+package com.example.storyapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.custom_item_view.view.*
+
+class ItemAdaptor(val storyTitles: Array<String>, val storyContents: Array<String>, val storyImages: Array<String>) : RecyclerView.Adapter<ItemAdaptor.ViewHolder>() {
+    class ViewHolder (itemView:View) : RecyclerView.ViewHolder(itemView) {
+        val cardTitle : TextView = itemView.cardTitle
+        val cardContent : TextView = itemView.cardContent
+        val cardImage : ImageView = itemView.cardImage
+        val view = itemView
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.custom_item_view,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.cardTitle.text=storyTitles[position]
+        holder.cardContent.text=storyContents[position]
+        Picasso.get().load(storyImages[position]).into(holder.cardImage)
+    }
+
+    override fun getItemCount(): Int = storyTitles.size
+
+
+}
