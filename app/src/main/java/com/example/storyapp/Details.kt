0facf779c493c31012228d0342d045d13be40e9b
Amir Salimi <108394058+TheSalimi@users.noreply.github.com>
2022-09-29 15:00:16 +0330

    Details.kt

--- app/src/main/java/com/example/storyapp/Details.kt
+++ app/src/main/java/com/example/storyapp/Details.kt
@@ -1,4 +1,30 @@
 package com.example.storyapp
 
-class Details {
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_details.*
+
+class Details :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+
+        val storyTitle = intent.getStringExtra("storyTitle")
+        val storyContent = intent.getStringExtra("storyContent")
+        val storyImage = intent.getStringExtra("storyImage")
+
+        supportActionBar?.title = storyTitle
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        Picasso.get().load(storyImage).into(story_Image)
+        storyDetails.text = storyContent
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == android.R.id.home) onBackPressed()
+        return super.onOptionsItemSelected(item)
+    }
 }
