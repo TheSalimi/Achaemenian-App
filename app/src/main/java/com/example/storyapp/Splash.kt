3b7ac0249b1dfc3e38df126a8cedecec15cc8b70
Amir Salimi <108394058+TheSalimi@users.noreply.github.com>
2022-10-01 20:30:13 +0330

    UI first edition completed

---
+++ app/src/main/java/com/example/storyapp/Splash.kt
@@ -0,0 +1,19 @@
+package com.example.storyapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Splash :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(applicationContext , MainActivity::class.java))
+        } , 3000)
+    }
+}
