9e3a64d0d06fae2ddc4cb82a66753da4ec2b03ec
Amir Salimi <108394058+TheSalimi@users.noreply.github.com>
2022-10-03 12:00:08 +0330

    Buttons UI

--- app/src/main/java/com/example/storyapp/ItemAdaptor.kt
+++ app/src/main/java/com/example/storyapp/ItemAdaptor.kt
@@ -10,59 +10,104 @@ import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.custom_item_view.view.*
 
-class ItemAdaptor(val storyTitles: Array<String>, val storyContents: Array<String>, val storyImages: Array<String>) : RecyclerView.Adapter<ItemAdaptor.ViewHolder>() {
-    class ViewHolder (itemView:View) : RecyclerView.ViewHolder(itemView) {
-        val cardTitle : TextView = itemView.cardTitle
-        val cardContent : TextView = itemView.cardContent
-        val cardImage : ImageView = itemView.cardImage
+class ItemAdaptor(
+    val storyTitles: Array<String>,
+    val storyContents: Array<String>,
+    val storyImages: Array<String>
+) : RecyclerView.Adapter<ItemAdaptor.ViewHolder>() {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val cardTitle: TextView = itemView.cardTitle
+        val cardContent: TextView = itemView.cardContent
+        val cardImage: ImageView = itemView.cardImage
         val view = itemView
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.custom_item_view,parent,false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.custom_item_view, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.cardTitle.text=storyTitles[position]
-        holder.cardContent.text=storyContents[position]
+        holder.cardTitle.text = storyTitles[position]
+        holder.cardContent.text = storyContents[position]
         Picasso.get().load(storyImages[position]).into(holder.cardImage)
 
 
 
-        holder.view.setOnClickListener{
-            val time : String
+        holder.view.setOnClickListener {
+            val time: String
+            val wikipediaLink : String
+            val youTubeLink :String
+            val britannica : String
+
             val intent = Intent(it.context, Details::class.java)
-            intent.putExtra("storyTitle",storyTitles[position])
-            intent.putExtra("storyContent",storyContents[position])
-            intent.putExtra("storyImage",storyImages[position])
+            intent.putExtra("storyTitle", storyTitles[position])
+            intent.putExtra("storyContent", storyContents[position])
+            intent.putExtra("storyImage", storyImages[position])
 
-            time =
-            if(position<=3)  "560_530 BC"
+            //cyrus
+            if (position <= 3) {
+                time = "560_530 BC"
+                wikipediaLink = "https://en.wikipedia.org/wiki/Cyrus_the_Great"
+                britannica = "https://www.britannica.com/biography/Cyrus-the-Great"
+                youTubeLink = "https://www.youtube.com/watch?v=wMbD7c-YlAE"
+            }
+            //darius I
+            else if (position > 3 && position < 8) {
+                time = "522–486 BC"
+                youTubeLink = "https://www.youtube.com/watch?v=10e5PghEh7o"
+                wikipediaLink = "https://en.wikipedia.org/wiki/Darius_the_Great"
+                britannica = "https://www.britannica.com/biography/Darius-I"
+            }
+            //cambyses
+            else if (position == 8) {
+                time = "530–522 BC"
+                wikipediaLink="https://en.wikipedia.org/wiki/Cambyses_I"
+                britannica = "https://www.britannica.com/biography/Cambyses-I"
+                youTubeLink = "https://www.youtube.com/watch?v=u9sYFNfrYhI"
+            }
+            //bardiya
+            else if (position == 9) {
+                time = "522 BC"
+                wikipediaLink = "https://en.wikipedia.org/wiki/Bardiya"
+                britannica = "https://www.britannica.com/biography/Bardiya"
+                youTubeLink = "https://www.youtube.com/watch?v=j2thzB5nLs0"
+            }
+            //xerxes I
+            else if (position >= 10 && position <= 13) {
+                time = "486-465 BC"
+            }
+            //artaxerxes I
+            else if (position == 14) {
+                time = "465–424 BC"
+            }
+            //xerxes II
+            else if (position == 15) {
+                time = "424 BC"
+            }
+            //darius II
+            else if (position == 16) {
+                time = "423–405 BC"
+            }
+            //artaxerxes II
+            else if (position == 17) {
+                time = "405–358 BC"
+            }
+            //artaxerxes III
+            else if (position == 18) {
+                time = "358–338 BC"
+            }
+            //artaxerxes IV
+            else if (position == 19) {
+                time = "338–336 BC"
+            }
+            //darius III
+            else {
+                time = "336–330 BC"
+            }
 
-            else if(position>3 && position<8) "522–486 BC"
-
-            else if(position==8) "530–522 BC"
-
-            else if(position==9)  "522 BC"
-
-            else if(position>=10 && position<=13)  "486-465 BC"
-
-            else if(position==14)  "465–424 BC"
-
-            else if(position==15) "424 BC"
-
-            else if(position==16) "423–405 BC"
-
-            else if(position==17) "405–358 BC"
-
-            else if(position==18) "358–338 BC"
-
-            else if(position==19)  "338–336 BC"
-
-            else "336–330 BC"
-
-            intent.putExtra("time",time)
+            intent.putExtra("time", time)
 
             holder.view.context.startActivity(intent)
         }
